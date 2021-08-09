efd78abc0cdbe7ddcdfa96b0e4dd9f7fcf0e9f46
David Rosenberg <dmr@davidrosenberg.me>
2021-08-08 19:46:09 -0500

    parser works

--- static/parseBiopsy.js
+++ static/parseBiopsy.js
@@ -5,100 +5,178 @@
  * @description : parseBiopsy
  */
 
-export default class parseBiopsy {
-}
+const fs = require('fs')
+const rawtext = fs.readFileSync('/Users/davidrosenberg/temp.prostatebiopsy.txt', 'utf8')
 
-const makeRegion = function(name="", cores=[0, 0] gleason=[0,0], max_involvement=0, ece=false, svi=false) {
+var makeRegion = function (
+  rname = "",
+  cores = [0, 1],
+  gleason = [0, 0],
+  involvement = 0,
+  ece = false,
+  svi = false
+) {
   var region = {
-    rname: name,
-    cores: [0, 0],
+    rname: rname,
+    cores: cores,
     gleason: gleason,
-    max_involvement: percent,
+    involvement: involvement,
     ece: ece,
-    svi: svi
+    svi: svi,
+  };
+  return region;
+};
+
+var sumCores = function (c1, c2) {
+  return [c1[0] + c2[0], c1[1] + c2[1]];
+};
+
+var higherGleason = function (g1, g2) {
+  if (g1[0] + g1[1] >= g2[0] + g2[1] && g1[0] >= g2[0]) {
+    return g1;
+  } else if (g1[0] + g1[1] < g2[0] + g2[1]) {
+    return g2;
+  } else {
+    return g1[0] > g2[0] ? g1 : g2;
   }
-}
+};
 
-const addNewRegion = function(newRegion, workingRegions) {
-  if 
+var addNewRegion = function (newRegion, workingRegions) {
+  if (Object.keys(workingRegions).includes(newRegion.rname)) {
+    var oldRegion = workingRegions[newRegion.rname];
+    workingRegions[newRegion.rname] = {
+      ...oldRegion,
+      cores: sumCores(oldRegion.cores, newRegion.cores),
+      gleason: higherGleason(oldRegion.gleason, newRegion.gleason),
+      involvement: Math.max(oldRegion.involvement, newRegion.involvement),
+      ece: oldRegion.ece || newRegion.ece,
+      svi: oldRegion.svi || newRegion.svi,
+    };
+  } else {
+    workingRegions[newRegion.rname] = newRegion;
+  }
+  return workingRegions;
+};
 
-const getRegion = function(text) {
-  const newRegion = makeRegion(
-    rname=getRegionName(text).region,
-    cores=getCoreCount(text).coreCount,
-    gleason=getGleasonScore(text).gleason,
-    max_involvement=getMaxInvolvement(text).involvement,
-    ece=getFeatures(text).ece,
-    svi=getFeatures(text).svi
-  );
+var getRegion = function (text) {
+  // console.log(`trying to make a region from "${text}"`);
+  const gleasonScore = getGleasonScore(text);
+  var newRegion;
+  if (gleasonScore[0] == 0) {
+    newRegion = makeRegion(getRegionName(text).rname);
+  } else {
+    newRegion = makeRegion(
+      getRegionName(text).rname,
+      getCoreCount(text).coreCount,
+      getGleasonScore(text).gleason,
+      getMaxInvolvement(text).involvement,
+      getFeatures(text).ece,
+      getFeatures(text).svi
+    );
+  }
+  // console.log(JSON.stringify(newRegion));
   return newRegion;
-}
+};
 
+var getFeatures = function (text) {
+  var features;
+  if (
+    /extraprostatic extension/.test(text) ||
+    /extracapsular exten/.test(text)
+  ) {
+    features = { ece: true };
+  } else {
+    features = { ece: false };
+  }
+  features.svi = /seminal vesicle invasion/.test(text);
+  return { ...features, err: null };
+};
 
-const getRegionName = function(text) {
+var getCoreCount = function (text) {
+  if (/adenocarcinoma/.test(text.toLowerCase())) {
+    return { coreCount: [1, 1], err: null };
+  }
+  return { coreCount: [0, 1], err: null };
+};
+
+var getGleasonScore = function (text) {
+  const matcher = new RegExp(/gleason.{1,10}([0-5]) ?\+ ?([0-5])/);
+  if (matcher.test(text)) {
+    var scoreMatch = text.match(matcher);
+    return { gleason: [Number(scoreMatch[1]), Number(scoreMatch[2])], err: null};
+  }
+  return { gleason: [0, 0], err: null}
+};
+
+var getRegionName = function (text) {
   var low_text = text.toLowerCase();
   if (/left/.test(low_text)) {
-    return {region: 'left', err: null};
-  } else if (/right/.test(low_text) ) {
-    return {region: 'right', err: null};
-  } else if (/mri/.test(low_text) ) {
-    if ( low_text.match(/#([0-9]+)/ )) {
-      const m = low_text.match(/#([0-9]+)/);
-      return {region: `MRI target #${m[1]}`, err: null};
+    return { rname: "left", err: null };
+  } else if (/right/.test(low_text)) {
+    return { rname: "right", err: null };
+  } else if (/mri/.test(low_text)) {
+    if (low_text.match(/#([0-9]+)/)) {
+      var m = low_text.match(/#([0-9]+)/);
+      return { rname: `MRI target #${m[1]}`, err: null };
     }
   }
-  return {region: null, err: new Error(`Could not identify region in text '${low_text}'`) }
-}
+  return {
+    region: null,
+    err: new Error(`Could not identify region in text '${low_text}'`),
+  };
+};
 
-const getGleasonScore = function(text) {
-  return {primary: 0, secondary: 0, err: null}
-}
+var getMaxInvolvement = function (text) {
+  const matcher = new RegExp(/[<|>]? ?([0-9]{1,3}) ?%/);
+  if (matcher.test(text)) {
+    const match = text.match(matcher);
+    return { involvement: Number(match[1]), err: null };
+  }
+  return { involvement: 0, err: null };
+};
 
-const getMaxInvolvement = function(text) {
-  return {involvement: 0, err: null}
-}
+const printSummary = function (workingRegions) {
+   console.log(JSON.stringify(workingRegions));
+};
 
+var parseBiopsy = function (rawtext) {
+  var lines = rawtext.split('\n');
 
+  var regions = {};
 
-const parseBiopsy = (rawtext) => {
-  const lines = rawtext.split('\n').map( (x) => {
-    x.replace('\n', '');
-  });
-  var regions = {}
-
-  var l;
+  var l = [];
   var next_l = null;
 
   var first_line_finder = /^\s*([A-Z]\)\s+.*)$/;
-  while (lines.length > 0) {
-    l = lines.shift();
-    if(first_line_finder.test(l)) {
+  startwhile: while (lines.length > 0) {
+    l = [lines.shift()];
+    next_l = null;
+    if (first_line_finder.test(l[0])) {
       // Starting a new section
-      while ( next_l === null ) {
+      while (next_l === null) {
         next_l = lines.shift();
-        if ( (first_line_finder.test(next_l)) || (next_l === "" ) ) {
+        if ( ( first_line_finder.test(next_l))  || (/^\S*$/.test(next_l)) ) {
           // we are starting a new section, put it back
-          lines.unshift(next_l)
-        } else if ( /igned/.test(next_l) ) {
-          // we're done.  Get out
-          regions = addNewRegion(getRegion(lines));
-          return printSummary(regions);
+          regions = addNewRegion(getRegion(l.join(' ').toLowerCase()), regions);
+          // console.log(JSON.stringify(regions) + "\n\n\n");
+          if ( first_line_finder.test(next_l)) {
+            lines.unshift(next_l);
+          }
+          continue startwhile;
+          // continue here;
         } else {
-          l = `${l} ${next_l}`
+          l.push(next_l);
           next_l = null;
         }
       }
-      regions = addNewRegion(getRegion(lines));
+      regions = addNewRegion(getRegion(l.join(' ').toLowerCase()), regions);
+      // console.log(JSON.stringify(regions) + "\n\n\n");
     }
   }
-  return printSummary(regions);
-}
+  regions = addNewRegion(getRegion(l.join(' ').toLowerCase()), regions);
+  // console.log(JSON.stringify(regions));
+  return { summary: printSummary(regions), regions: regions };
+};
 
-
-
-
-
-
-
-
+parseBiopsy(rawtext);
 
