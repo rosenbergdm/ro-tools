99d966918d2d7b0c325ba00c1483278e5c5e118c
David Rosenberg <dmr@davidrosenberg.me>
2021-09-06 21:03:11 -0500

    updated date checker

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -3,8 +3,8 @@
 pbpaste=$(pbpaste && echo) 
 
 _systype=$(uname)
-case "$_system" in 
-  Darwin)
+case "$_systype" in 
+  Darwin* | darwin* )
     export AWK=/usr/local/bin/gawk
     export BASENAME=/usr/local/bin/gbasename
     export DATE=/usr/local/bin/gdate
@@ -28,8 +28,8 @@ case "$_system" in
     export GREP=/usr/local/bin/ggrep
     export EGREP=/usr/local/bin/gegrep
     export PGREP=/usr/local/bin/ggrep\ -P
-    export TOUCH=/usr/local/bin/gtouch ;;
-
+    export TOUCH=/usr/local/bin/gtouch
+    ;; 
   Linux)
     export AWK=$(which gawk || which awk)
     export BASENAME=$(which basename)
@@ -48,42 +48,49 @@ case "$_system" in
     export ECHO=$(which echo)
     export PRINTF=$(which printf)
     export WHICH=$(which which)
-    export curl=$(which curl)
+    export CURL=$(which curl)
     export REALPATH=$(which realpath)
     export STAT=$(which stat)
     export GREP=$(which grep)
     export EGREP=$(which egrep)
     export PGREP="$(which grep) -P"
     export TOUCH=$(which touch) ;;
-
-    esac 
+  *) 
+    echo "unknown system type '$_systype'"
+    exit 1
+    ;;
+esac 
 export commands=( AWK BASENAME DATE DIRNAME FIND GIT GUNZIP GZIP \
   MKTEMP PERL PG_DUMP PSQL READLINK SED ECHO PRINTF WHICH CURL \
   STAT REALPATH TOUCH )
 
 
 get_date_struct() {
-  usage=$'Use like this: > declare -A datestruct="$(eval "$(get_date_struct 02/02/21)")"'
+  # unset $(declare -A | $GREP dstruct |  perl -p -e 's/^.*declare -.{1,2} ([A-Za-z]*dstruct[A-Za-z]*).*/\1/g')
+  # unset $(declare -A | $GREP d1 |  perl -p -e 's/^.*declare -.{1,2} ([A-Za-z]*d1[A-Za-z]*).*/\1/g')
+  # unset $(declare -A | $GREP d2 |  perl -p -e 's/^.*declare -.{1,2} ([A-Za-z]*d2[A-Za-z]*).*/\1/g')
+  usage=$'Use like this: > declare -A datestruct=$(eval "$(get_date_struct 02/02/21)")'
   if [[ "z$1" == "z" ]]; then
-    $PRINTF "%s\n\n" "$usage"
-    return
+    $PRINTF "%s\n\n" "$usage" > /dev/stderr
+    return 4
   fi
   declare -a parts
   parts=( $( echo "$1" | sed -e 's/\//\n/g' ) )
-  if [[ ${#parts} -gt 2 ]]; then
+  if [[ ${#parts[@]} -gt 3 ]]; then
+    # printf "%s\n\n" "\${#parts} = '${#parts[@]}'" > /dev/stderr
     # we have a range: TODO
     $PRINTF "%s\n" "An error occured" > /dev/stderr
     exit 1
-  elif [[ ${#parts} == 2 ]]; then 
-    year="$( echo "20${parts[2]}" | $PERL -p -e  's/^.*(....)$/\1/g' )"
-    month="$( echo "0${parts[0]}" | $PERL -p -e 's/^.*(..)$/\1/g' )"
-    day="$(echo "0${parts[1]}" | $PERL -p -e 's/^.*(..)$/\1/g' )"
-  elif [[ ${#parts} == 1 ]]; then
-    year="$( echo "20${parts[1]}" | $PERL -p -e  's/^.*(....)$/\1/g' )"
-    month="$( echo "0${parts[0]}" | $PERL -p -e 's/^.*(..)$/\1/g' )"
+  elif [[ ${#parts[@]} == 3 ]]; then 
+    year="$( echo "20${parts[2]}" | $SED -e  's/^.*\(....\)$/\1/g' )"
+    month="$( echo "0${parts[0]}" | $SED -e 's/^.*\(..\)$/\1/g' )"
+    day="$(echo "0${parts[1]}" | $SED -e 's/^.*\(..\)$/\1/g' )"
+  elif [[ ${#parts[@]} == 2 ]]; then
+    year="$( echo "20${parts[1]}" | $SED -e  's/^.*\(....\)$/\1/g' )"
+    month="$( echo "0${parts[0]}" | $SED -e 's/^.*\(..\)$/\1/g' )"
     day="-1"
-  elif [[ ${#parts} == 0 ]]; then
-    year="$( echo "20${parts[0]}" | $PERL -p -e  's/^.*(....)$/\1/g' )"
+  elif [[ ${#parts[@]} == 1 ]]; then
+    year="$( echo "20${parts[0]}" | $SED -e  's/^.*\(....\)$/\1/g' )"
     month="-1"
     day="-1"
   fi
@@ -91,81 +98,98 @@ get_date_struct() {
   dstruct[year]=$year
   dstruct[day]=$day
   dstruct[month]=$month
-  typeset -A | grep dstruct | sed -e 's/^.*dstruct=//g'
+  typeset -A | $GREP dstruct | sed -e 's/^.*dstruct=//g'
+  unset dstruct parts day year month
+  return 0
 }
 
 lines_in_order() {
-  declare -A d1
-  declare -A d2
-  line1="$(echo "$1" | $PERL -p -e 's/(^\W*[0-9\/\-]{4,20})( ?- ?[0-9\/\-]{4,20})?:?\W.*/\1\2/g')"
-  $PRINTF "%s\n" "line1='$line1'"
-  line2="$(echo "$2" | $PERL -p -e 's/(^\W*[0-9\/\-]{4,20})( ?- ?[0-9\/\-]{4,20})?:?\W.*/\1\2/g')"
-  $PRINTF "%s\n" "line2='$line2'"
-  eval "declare -A d1=$(get_date_struct "$line1" )"
-  eval "declare -A d2=$(get_date_struct "$line2" )"
+  unset line1 line2 d1 d2 dstruct parts
+  line1="$(echo "$1" | $PERL -p -e 's/(^\W*[0-9\/\-]{4,20})( ?- ?[0-9\/\-]{4,20})?:\W.*/\1\2/g' | sed -e 's/:.*$//g' )"
+  line2="$(echo "$2" | $PERL -p -e 's/(^\W*[0-9\/\-]{4,20})( ?- ?[0-9\/\-]{4,20})?:\W.*/\1\2/g' | sed -e 's/:.*$//g' )"
+  eval declare -A d1="$(get_date_struct "$line1" )"
+  eval declare -A d2="$(get_date_struct "$line2" )"
+  # typeset -A > /dev/stderr
   if [[ ${d2[year]} -gt ${d1[year]} ]]; then
-    $PRINTF "%s\n" 1
-    # $PRINTF "%s $LINENO year greater\n" 1
+    $PRINTF "%s\n" 0 
+    # echo 112  > /dev/stderr && typeset -A > /dev/stderr
   elif [[ ${d2[year]} -lt ${d1[year]} ]]; then 
-    $PRINTF "%s\n" 0
-    # $PRINTF "%s $LINENO year less \n" 0
-  elif [[ ${d2[month]} -gt ${d1[year]} ]]; then
     $PRINTF "%s\n" 1
-    # $PRINTF "%s $LINENO month greater\n" 1
-  elif [[ ${d2[month]} -lt ${d1[year]} ]]; then 
-    $PRINTF "%s\n" 0
-    # $PRINTF "%s $LINENO month less \n" 0
-  elif [[ ${d2[month]} -eq -1  ]] || [[  ${d1[month]} -eq -1 ]]; then
-    $PRINTF "%s\n" 1
-    # $PRINTF "%s $LINENO year equal, and no month comparison possible\n" 1
-  elif [[ ${d2[day]} -gt ${d1[year]} ]]; then
-    $PRINTF "%s\n" 1
-    # $PRINTF "%s $LINENO day greater\n" 1
-  elif [[ ${d2[day]} -lt ${d1[year]} ]]; then 
-    $PRINTF "%s\n" 0
-    # $PRINTF "%s $LINENO day less \n" 0
-  elif [[ ${d2[day]} -eq -1  ]] || [[  ${d1[day]} -eq -1 ]]; then
-    $PRINTF "%s\n" 1
-    # $PRINTF "%s $LINENO year, month equal, and no day comparison possible\n" 1
+    # echo 115  > /dev/stderr && typeset -A
   else
-    $PRINTF "%s \n" "THIS SHOULDN'T HAPPEN"
+    # Years are equal.  Must test for missing month first
+    if [[ ${d2[month]} -eq -1  ]] || [[  ${d1[month]} -eq -1 ]]; then
+      $PRINTF "%s\n" 0
+      # echo 120 > /dev/stderr && typeset -A > /dev/stderr
+    elif [[ ${d2[month]} -gt ${d1[month]} ]]; then
+      $PRINTF "%s\n" 0
+      # echo 123 > /dev/stderr && typeset -A > /dev/stderr
+    elif [[ ${d2[month]} -lt ${d1[month]} ]]; then 
+      $PRINTF "%s\n" 1
+      # echo 126 > /dev/stderr && typeset -A > /dev/stderr
+    else
+      # Years and months are equal.  Must test for missing day first
+      if [[ ${d2[day]} -eq -1  ]] || [[  ${d1[day]} -eq -1 ]]; then
+        $PRINTF "%s\n" 0
+        # echo 131 > /dev/stderr && typeset -A > /dev/stderr
+      elif [[ ${d2[day]} -gt ${d1[day]} ]]; then
+        $PRINTF "%s\n" 0
+        # echo 134 > /dev/stderr && typeset -A > /dev/stderr
+      elif [[ ${d2[day]} -lt ${d1[day]} ]]; then 
+        $PRINTF "%s\n" 1
+        # echo 137 > /dev/stderr && typeset -A > /dev/stderr
+      else
+        $PRINTF "%s\n" 1
+        # echo 140 > /dev/stderr && typeset -A > /dev/stderr
+      fi
+    fi
   fi
 }
 
-
-check_date_sort () 
+check_date_sort.old () 
 { 
     f1=$($MKTEMP);
     f2=$($MKTEMP);
-    pbpaste | $PGREP '^\d{1,2}\/\d{1,2}\/\d{2,4}' | grep -P -v '^\s*$' > $f1
-    pbpaste | grep -P '^\d{2}\/\d{2}\/\d{4}' | grep -P -v '^\s*$' | sort -t '/' -k3,4g -k1,2g -k2,2g > $f2
+    pbpaste | $P$GREP '^\d{1,2}\/\d{1,2}\/\d{2,4}' | $GREP -P -v '^\s*$' > $f1
+    pbpaste | $GREP -P '^\d{2}\/\d{2}\/\d{4}' | $GREP -P -v '^\s*$' | sort -t '/' -k3,4g -k1,2g -k2,2g > $f2
     res=$(diff $f1 $f2 >/dev/null 2>&1; echo $?);
-    if [[ $res > 0 ]]; then
+    if [[ $res -gt 0 ]]; then
        diff $f1 $f2;
     fi;
     return $res
 }
 
-check_date_sort2() {
-  f1=$($MKTEMP);
-  f2=$($MKTEMP);
-  pbpaste | grep -v '^\W+$' > $f1
-  line1=$(head -n1 $f1)
+check_date_sort() {
+  f1=$($MKTEMP -t ro-tools.datecheck.tmpXXXXXX );
+  f2=$($MKTEMP -t ro-tools.datecheck.tmpXXXXXX );
+  if [[ "z$1" == "z" ]]; then
+    pbpaste | $GREP -v '^\W*$' | $PERL -p -e 's/:.*$//g' > $f1
+    _src=pasteboard
+  else
+    cat "$1" | $GREP -v '^\W*$' | $PERL -p -e 's/:.*$//g' > $f1
+    _src="$1"
+  fi
+  l1=$(head -n1 $f1)
   tail -n $(echo "$(wc -l $f1 | $AWK '{print $1}')-1" | bc ) $f1 > $f2
   mv $f2 $f1
   
-  while IFS= read -r line2; do
-    linesok=$(lines_in_order "$line1" "$line2")
-    if [[ $linesok -eq 1 ]]; then
-      echo "ok"
+  while IFS= read -r l2; do
+    # echo "l1='$l1'; l2='$l2'"
+    linesok=$(lines_in_order "$l1" "$l2")
+    # echo "$linesok"
+    if [[ "$linesok" == "0" ]]; then
+      linesok=0
     else
-      $PRINTF "%s\n\n" "ERROR, lines '$line1' and '$line2' are out of order" > /dev/stderr
+      $PRINTF "%s\n\n" "ERROR, lines '$l1' and '$l2' are out of order" > /dev/stderr
+      rm -Rf "$f1" "$f2"
       return 1
     fi
-    line1="$line2"
+    l1="$l2"
+    unset l2
   done < $f1
-
-  $PRINTF "%s\n" "0"
+  rm -Rf "$f1" "$f2"
+  $PRINTF "%s\n\n" "$_src lines were all listed in order"
+  return 0
 }
 
 add_textblock() {
